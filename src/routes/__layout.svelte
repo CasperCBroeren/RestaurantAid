05f43fbc4725f00c04d813fdd465b754ae37e9cf
Casper Broeren <c.broeren@gmail.com>
2022-03-21 22:36:13 +0100

    Styling fix

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,15 +4,8 @@
    
   </script>
   
-  <main class="p-10 bg-base-100 h-screen w-screen">
+  <main class="p-10">
      <Menu />
      <slot></slot>
   </main>
-  
-  <style>
-    main {
-      background: gainsboro;
-      background-image: radial-gradient(rgb(189, 189, 189) 1px, transparent 0);
-      background-size: 10px 10px;
-    }
-  </style>
+ 
